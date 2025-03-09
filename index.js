3f21d174cae0b75211cad7ed3f027515581eb3ad
Justin mccutcheon <jt.mccutcheon@live.com>
2025-03-08 19:36:35 -0500

    formatting

--- index.js
+++ index.js
@@ -3,47 +3,52 @@ const { GatewayIntentBits, Client } = require('discord.js');
 configDotenv()
 
 const main = async () => {
-    try {
-        const res = await fetch('https://insignia.live/halo2')
-        const text = await res.text()
-        const arr = text.split('\n')
-        const hardcoreIndex = arr.findIndex(i => i.includes('Team Hardcore'))
-        const slice = arr.slice(hardcoreIndex, hardcoreIndex + 10)
-        const playerCount = parseInt(slice.find(s => s.includes('Players')).trim()[0])
+  try {
+    const res = await fetch('https://insignia.live/halo2')
+    const text = await res.text()
+    const arr = text.split('\n')
+    const hardcoreIndex = arr.findIndex(i => i.includes('Team Hardcore'))
+    const slice = arr.slice(hardcoreIndex, hardcoreIndex + 10)
+    const playerCount = parseInt(slice.find(s => s.includes('Players')).trim()[0])
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    console.log({playerCount, currentTime})
+    
+    if (playerCount >= 4) { 
+      const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+      client.login(process.env.DISCORD_AUTH_TOKEN);
+      client.on('ready', () => {
 
-        if (playerCount >= 4) { 
-            const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-            client.login(process.env.DISCORD_AUTH_TOKEN);
+        client.guilds.cache.map(async guild => {
+          await guild.channels
+          .fetch()
+          .then(async channels => {
 
-            client.on('ready', () => {
+            const textChannels = channels.filter(
+              channel => channel.type === 0,
+            );
+            const channel = textChannels.filter(textChannel =>
+              textChannel.name.includes('h2'),
+            );
+            console.log({server: guild.name, channels: channel.map(c => c.name)})
+            // if(guild.name === 'Keylords') { // if testing
+            channel.map(c => c.send(`Team Hardcore has ${playerCount} players searching.`));
+            // }
+          })
+          .catch(err => {
+            console.log({err})
+          });
+  
+        })
 
-              client.guilds.cache.map(async guild => {
-                
-                await guild.channels
-                .fetch()
-                .then(async channels => {
-
-                  const textChannels = channels.filter(
-                    channel => channel.type === 0,
-                  );
-                  const channel = textChannels.filter(textChannel =>
-                    textChannel.name.includes('h2'),
-                  );
-                  channel.map(c => c.send(`Team Hardcore has ${playerCount} players searching.`));
-                })
-                .catch(err => {
-                  console.log({err})
-                });
-        
-              })
-            });
-
-        }
-
-    } catch (error) {
-        console.log({error})
+      });
     }
+  } catch (error) {
+    console.log({error})
+  }
 }
+
+main()
 
 setInterval(() => {
   main()
